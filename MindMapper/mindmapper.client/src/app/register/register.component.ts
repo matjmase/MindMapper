cc32f5a34b2ba734f241e9dd7149936750d1bb4f
matjmase <67738268+matjmase@users.noreply.github.com>
2025-01-04 00:14:44 -0600

    Register Password Validation

--- MindMapper/mindmapper.client/src/app/register/register.component.ts
+++ MindMapper/mindmapper.client/src/app/register/register.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidatorFn,
+  AbstractControl,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../api';
 import { SessionService } from '../services/session.service';
+
+function validatePasswords(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const formGroup = <FormGroup>control;
+
+    const password = formGroup.controls['password'];
+    const passwordConfirm = formGroup.controls['passwordConfirm'];
+
+    if (
+      password.dirty &&
+      passwordConfirm.dirty &&
+      password.value !== passwordConfirm.value
+    ) {
+      return {
+        passwordsNotMatching: true,
+      };
+    }
+
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-register',
@@ -10,12 +37,17 @@ import { SessionService } from '../services/session.service';
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  public registerForm = new FormGroup({
-    email: new FormControl<string>('', [Validators.required]),
-    userName: new FormControl<string>('', [Validators.required]),
-    password: new FormControl<string>('', [Validators.required]),
-    passwordConfirm: new FormControl<string>('', [Validators.required]),
-  });
+  public registerForm = new FormGroup(
+    {
+      email: new FormControl<string>('', [Validators.required]),
+      userName: new FormControl<string>('', [Validators.required]),
+      password: new FormControl<string>('', [Validators.required]),
+      passwordConfirm: new FormControl<string>('', [Validators.required]),
+    },
+    {
+      validators: [validatePasswords()],
+    }
+  );
 
   constructor(
     private authService: AuthenticationService,
@@ -23,7 +55,7 @@ export class RegisterComponent {
     private router: Router
   ) {}
 
-  public login(): void {
+  public register(): void {
     this.authService
       .apiAuthenticationRegisterPost({
         email: this.registerForm.value.email,
